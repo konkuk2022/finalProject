00f15a3daa810f0e3b96d53cd0bc2a78a2109c95
inuinseoul <inwoo7233@naver.com>
2022-05-12 20:04:35 +0900

    add: 앱 기본 세팅

---
+++ Moviemotion/Moviemotion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moviemotion
+//
+//  Created by Inwoo Park on 2022/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
